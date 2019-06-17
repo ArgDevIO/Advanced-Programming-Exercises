d90e47f2b9e5b76bd5c189b6642e3735ac8a9e15
ArgDevIO <argdev@outlook.com>
2019-06-17 02:39:22 +0200

    Initial Commit: Just implemented the Item interface and 2 classes ExtraItem & PizzaItem, still work to do...

---
+++ src/com/pizzeria/PizzaOrderTest.java
@@ -0,0 +1,163 @@
+package com.pizzeria;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+interface Item {
+    int getPrice();
+}
+
+//Exceptions start
+class InvalidExtraTypeException extends Exception {
+    InvalidExtraTypeException(String message) {
+        super(message);
+    }
+}
+
+class InvalidPizzaTypeException extends Exception {
+    public InvalidPizzaTypeException(String message) {
+        super(message);
+    }
+}
+//Exceptions end
+
+class ExtraItem implements Item  {
+    private String type;
+
+    ExtraItem(String type) throws InvalidExtraTypeException {
+        List<String> allowedItems = Arrays.asList("Coke", "Ketchup");
+        if (allowedItems.contains(type))
+            this.type = type;
+        else
+            throw new InvalidExtraTypeException(type + " is invalid ExtraItem");
+    }
+
+    @Override
+    public int getPrice() {
+        return 0;
+    }
+}
+
+class PizzaItem implements Item {
+    private String type;
+
+    PizzaItem(String type) throws InvalidPizzaTypeException {
+        List<String> allowedItems = Arrays.asList("Standard", "Pepperoni", "Vegetarian");
+        if (allowedItems.contains(type))
+            this.type = type;
+        else
+            throw new InvalidPizzaTypeException(type + " is invalid PizzaItem");
+    }
+
+    @Override
+    public int getPrice() {
+        return 0;
+    }
+}
+
+public class PizzaOrderTest {
+
+    public static void main(String[] args) {
+        Scanner jin = new Scanner(System.in);
+        int k = jin.nextInt();
+        if (k == 0) { //test Item
+            try {
+                String type = jin.next();
+                String name = jin.next();
+                Item item = null;
+                if (type.equals("Pizza")) item = new PizzaItem(name);
+                else item = new ExtraItem(name);
+                System.out.println(item.getPrice());
+            } catch (Exception e) {
+                System.out.println(e.getClass().getSimpleName());
+            }
+        }
+        if (k == 1) { // test simple order
+            Order order = new Order();
+            while (true) {
+                try {
+                    String type = jin.next();
+                    String name = jin.next();
+                    Item item = null;
+                    if (type.equals("Pizza")) item = new PizzaItem(name);
+                    else item = new ExtraItem(name);
+                    if (!jin.hasNextInt()) break;
+                    order.addItem(item, jin.nextInt());
+                } catch (Exception e) {
+                    System.out.println(e.getClass().getSimpleName());
+                }
+            }
+            jin.next();
+            System.out.println(order.getPrice());
+            order.displayOrder();
+            while (true) {
+                try {
+                    String type = jin.next();
+                    String name = jin.next();
+                    Item item = null;
+                    if (type.equals("Pizza")) item = new PizzaItem(name);
+                    else item = new ExtraItem(name);
+                    if (!jin.hasNextInt()) break;
+                    order.addItem(item, jin.nextInt());
+                } catch (Exception e) {
+                    System.out.println(e.getClass().getSimpleName());
+                }
+            }
+            System.out.println(order.getPrice());
+            order.displayOrder();
+        }
+        if (k == 2) { // test order with removing
+            Order order = new Order();
+            while (true) {
+                try {
+                    String type = jin.next();
+                    String name = jin.next();
+                    Item item = null;
+                    if (type.equals("Pizza")) item = new PizzaItem(name);
+                    else item = new ExtraItem(name);
+                    if (!jin.hasNextInt()) break;
+                    order.addItem(item, jin.nextInt());
+                } catch (Exception e) {
+                    System.out.println(e.getClass().getSimpleName());
+                }
+            }
+            jin.next();
+            System.out.println(order.getPrice());
+            order.displayOrder();
+            while (jin.hasNextInt()) {
+                try {
+                    int idx = jin.nextInt();
+                    order.removeItem(idx);
+                } catch (Exception e) {
+                    System.out.println(e.getClass().getSimpleName());
+                }
+            }
+            System.out.println(order.getPrice());
+            order.displayOrder();
+        }
+        if (k == 3) { //test locking & exceptions
+            Order order = new Order();
+            try {
+                order.lock();
+            } catch (Exception e) {
+                System.out.println(e.getClass().getSimpleName());
+            }
+            try {
+                order.addItem(new ExtraItem("Coke"), 1);
+            } catch (Exception e) {
+                System.out.println(e.getClass().getSimpleName());
+            }
+            try {
+                order.lock();
+            } catch (Exception e) {
+                System.out.println(e.getClass().getSimpleName());
+            }
+            try {
+                order.removeItem(0);
+            } catch (Exception e) {
+                System.out.println(e.getClass().getSimpleName());
+            }
+        }
+    }
+
+}
